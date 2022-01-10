6d271fd69056b888f83f6da8f9a321647f04ec7b
Kevin Chakornsiri <kevin.chakorn@gmail.com>
2022-01-09 23:22:55 -0500

    Finish step-4

--- module1_hw_wrapper/platform.tcl
+++ module1_hw_wrapper/platform.tcl
@@ -25,3 +25,9 @@ domain active {zynq_fsbl}
 bsp reload
 platform generate
 platform generate
+platform active {module1_hw_wrapper}
+domain active {standalone_ps7_cortexa9_0}
+bsp reload
+domain active {zynq_fsbl}
+bsp reload
+platform generate -domains 
