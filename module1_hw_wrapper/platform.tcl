460573562037519f0e4a935631552b6b7b9e4336
Kevin Chakornsiri <kevin.chakorn@gmail.com>
2022-01-09 20:47:27 -0500

    Initial commit

---
+++ module1_hw_wrapper/platform.tcl
@@ -0,0 +1,26 @@
+# 
+# Usage: To re-create this platform project launch xsct with below options.
+# xsct /home/thayer/workspace/module1_hw_wrapper/platform.tcl
+# 
+# OR launch xsct and run below command.
+# source /home/thayer/workspace/module1_hw_wrapper/platform.tcl
+# 
+# To create the platform in a different location, modify the -out option of "platform create" command.
+# -out option specifies the output directory of the platform project.
+
+platform create -name {module1_hw_wrapper}\
+-hw {/home/thayer/workspace/module1/module1_hw_wrapper.xsa}\
+-out {/home/thayer/workspace}
+
+platform write
+domain create -name {standalone_ps7_cortexa9_0} -display-name {standalone_ps7_cortexa9_0} -os {standalone} -proc {ps7_cortexa9_0} -runtime {cpp} -arch {32-bit} -support-app {hello_world}
+platform generate -domains 
+platform active {module1_hw_wrapper}
+domain active {zynq_fsbl}
+domain active {standalone_ps7_cortexa9_0}
+platform generate -quick
+bsp reload
+bsp reload
+domain active {zynq_fsbl}
+bsp reload
+platform generate
---
+++ module1_vts_system/_ide/scripts/systemdebugger_module1_vts_system_standalone.tcl
@@ -0,0 +1,28 @@
+# Usage with Vitis IDE:
+# In Vitis IDE create a Single Application Debug launch configuration,
+# change the debug type to 'Attach to running target' and provide this 
+# tcl script in 'Execute Script' option.
+# Path of this script: /home/thayer/workspace/module1_vts_system/_ide/scripts/systemdebugger_module1_vts_system_standalone.tcl
+# 
+# 
+# Usage with xsct:
+# To debug using xsct, launch xsct and run below command
+# source /home/thayer/workspace/module1_vts_system/_ide/scripts/systemdebugger_module1_vts_system_standalone.tcl
+# 
+connect -url tcp:127.0.0.1:3121
+targets -set -nocase -filter {name =~"APU*"}
+rst -system
+after 3000
+targets -set -filter {jtag_cable_name =~ "Digilent Zybo Z7 210351AF242FA" && level==0 && jtag_device_ctx=="jsn-Zybo Z7-210351AF242FA-13722093-0"}
+fpga -file /home/thayer/workspace/module1_vts/_ide/bitstream/module1_hw_wrapper.bit
+targets -set -nocase -filter {name =~"APU*"}
+loadhw -hw /home/thayer/workspace/module1_hw_wrapper/export/module1_hw_wrapper/hw/module1_hw_wrapper.xsa -mem-ranges [list {0x40000000 0xbfffffff}] -regs
+configparams force-mem-access 1
+targets -set -nocase -filter {name =~"APU*"}
+source /home/thayer/workspace/module1_vts/_ide/psinit/ps7_init.tcl
+ps7_init
+ps7_post_config
+targets -set -nocase -filter {name =~ "*A9*#0"}
+dow /home/thayer/workspace/module1_vts/Debug/module1_vts.elf
+configparams force-mem-access 0
+bpadd -addr &main
